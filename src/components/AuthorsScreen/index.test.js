da120eecfdd0e9ad833a42387a2d1918e6e88940
DragonSpirit <cat_vip@mail.ru>
2019-04-14 23:13:45 +0300

    Improve test coverage

--- src/components/AuthorsScreen/index.test.js
+++ src/components/AuthorsScreen/index.test.js
@@ -2,12 +2,21 @@ import React from 'react';
 import AuthorsScreen from './';
 import renderer from 'react-test-renderer';
 
+const fetch = jest.fn()
+
 test('empty renders correctly', () => {
-  const tree = renderer.create(<AuthorsScreen />).toJSON();
+  const tree = renderer.create(<AuthorsScreen fetchPosts={fetch}/>).toJSON();
   expect(tree).toMatchSnapshot();
 });
 
 test('non empty renders correctly', () => {
-  const tree = renderer.create(<AuthorsScreen authors={['test']} />).toJSON();
+  const tree = renderer.create(<AuthorsScreen authors={['test']} fetchPosts={fetch} />).toJSON();
   expect(tree).toMatchSnapshot();
+})
+
+test('should delete author', () => {
+  const deleteAuthor = jest.fn();
+  const tree = renderer.create(<AuthorsScreen authors={['test']} deleteAuthor={deleteAuthor} fetchPosts={fetch}/>);
+  tree.getInstance().deleteUser('test');
+  expect(deleteAuthor).toHaveBeenCalledWith('test');
 })
--- src/components/FavoriteScreen/index.test.js
+++ src/components/FavoriteScreen/index.test.js
@@ -7,3 +7,24 @@ test('renders correctly', () => {
   const tree = renderer.create(<FavoriteScreen />).toJSON();
   expect(tree).toMatchSnapshot();
 });
+
+test('renders correctly with empty feed but exists authors', () => {
+  const tree = renderer.create(<FavoriteScreen authors={['test']} feed={[]} />).toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+test('renders correctly with non empty feed and authors', () => {
+  const feed = [{
+    author: 'test',
+    id: 1,
+    did: 1,
+    title: 'Test',
+    text: 'Test',
+    ts: 1,
+    url: 'local.url',
+    tags: 'tag',
+    isFavorite: false,
+  }]
+  const tree = renderer.create(<FavoriteScreen authors={['test']} feed={feed} />).toJSON();
+  expect(tree).toMatchSnapshot();
+});
